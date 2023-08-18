73bc79c25f960d993464cae7c634a9483025e0ae
alfredW117 <21301150@uttt.edu.mx>
2023-08-18 13:24:05 -0600

    cambios database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import psycopg2
 
 #hacer conexion bd
 def obtener_conexion():
-    conn = psycopg2.connect(host='localhost', port=5432, database='monitoreo_servidores', user='postgres', password='cisco123')
+    conn = psycopg2.connect(host='ec2-44-212-250-48.compute-1.amazonaws.com', port=5432, database='daprs7gajn8iue', user='uxkmtjlhyxabiw', password='57f31dea73be201545726868c0a3ddffe9d07bc66c2500113f40b24224f4a38c')
     if conn is None:
         print('Error al conectar a la base de datos')
     else:
